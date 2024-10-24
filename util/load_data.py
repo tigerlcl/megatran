39ed55024a28fec6cce7b216592240f8d06a047e
Tiger Li <tigerlicuhk@gmail.com>
2024-10-24 03:27:41 +0000

    add more tests

--- util/load_data.py
+++ util/load_data.py
@@ -7,8 +7,20 @@ DATASET_DICT = {
     },
     "bingquery-logs": {
         "path": "./data/TDE-v2/benchmark-bing-query-logs"
+    },
+    "unit_convert": {
+        "path": "./data/TDE-v2/benchmark-bing-query-logs"
+    },
+    "headcase": {
+        "path": "./data/TDE-v2/benchmark-headcase"
+    },
+    "prep-software": {
+        "path": "./data/TDE-v2/benchmark-FF-Trifacta-GoogleRefine"
     }
 }
+
+# for non-special datasets, the data is stored in json files
+JSON_NORMAL_GROUP = ["stackoverflow", "headcase", "prep-software"]
 
 
 def load_dataset_by_name(dataset_name):
@@ -19,20 +31,20 @@ def load_dataset_by_name(dataset_name):
         raise ValueError(f"Dataset '{dataset_name}' not found in the configuration.")
     
     # Load the dataset using the datasets library
-    if dataset_name == "stackoverflow":
-        dataset =  _load_stackoverflow(dataset_info)
+    if dataset_name in JSON_NORMAL_GROUP:
+        dataset =  _load_json_files(dataset_info)
     elif dataset_name == "bingquery-logs":
         dataset = _load_bingquery_logs(dataset_info)
+    elif dataset_name == "unit_convert":
+        dataset = _load_unit_convert(dataset_info)
 
     # sort dataset by file_path
     dataset = sorted(dataset, key=lambda x: x['file_path'])
 
     return dataset
 
-def _load_stackoverflow(dataset_info: dict):
-    # source from TDE-v2/benchmark-stackoverflow
+def _load_json_files(dataset_info: dict):
     data = []
-    
     for file in os.listdir(dataset_info["path"]):
         if file.endswith('.json'):
             json_fp = os.path.join(dataset_info["path"], file)
@@ -49,6 +61,20 @@ def _load_bingquery_logs(dataset_info: dict):
     
     for file in os.listdir(dataset_info["path"]):
         if file.startswith('semantic_') and file.endswith('.json'):
+            json_fp = os.path.join(dataset_info["path"], file)
+            with open(json_fp, 'r') as f:
+                obj = json.load(f)
+                obj['file_path'] = json_fp
+                data.append(obj)
+
+    return data
+
+def _load_unit_convert(dataset_info: dict):
+    # source from TDE-v2/benchmark-unit-convert: filter by prefix 'semantic_'
+    data = []
+    
+    for file in os.listdir(dataset_info["path"]):
+        if file.startswith('unit_') and file.endswith('.json'):
             json_fp = os.path.join(dataset_info["path"], file)
             with open(json_fp, 'r') as f:
                 obj = json.load(f)
